c9d515f3d7c1707f463e96fba411b709a45c6bf0
Valou0539 <valentin.gindre@gmail.com>
2023-11-25 12:39:20 +0100

    update index and register page

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,75 @@
 <template>
-  <div></div>
+  <main class="mx-4">
+    <section class="mt-12 mb-20 max-w-xl lg:max-w-4xl mx-auto lg:flex gap-6 items-center">
+      <div class="mb-8 lg:mb-0">
+        <h1 class="text-[40px] font-header font-bold mb-4 lg:text-[67px] leading-tight">Calculer votre moyenne
+          simplement</h1>
+        <p class="text-xl lg:text-[22px] w-4/5">
+          Entrez vos notes de BUT informatique sur notre application pour un calcul rapide et sans effort de votre
+          moyenne.
+        </p>
+      </div>
+      <form class="lg:w-[356px] flex-shrink-0" @submit.prevent>
+        <CustomInput
+            @input="console.log(email)"
+            name="pseudo"
+            label="Pseudonyme"
+            type="text"
+            placeholder="Pseudonyme"
+            v-model="email"
+            class="mb-4"
+            autoComplete="username"
+        />
+        <CustomInput
+            @input="console.log(password)"
+            name="password"
+            label="Mot de passe"
+            type="password"
+            placeholder="Mot de passe"
+            v-model="password"
+            class="mb-8"
+            autoComplete="password"
+        />
+        <div class="sm:flex items-center gap-4">
+          <button
+              type="submit"
+              class="w-full flex-shrink-0 bg-primary-button dark:bg-primary-button-dark text-primary-button-text dark:text-primary-button-text-dark text-lg px-4 py-3 mb-4 sm:w-auto sm:mb-0"
+          >
+            Se connecter
+          </button>
+          <NuxtLink to="/register" class="hover:underline underline-offset-2">
+            Je n’ai pas de compte, j’en crée un
+          </NuxtLink>
+        </div>
+
+      </form>
+    </section>
+    <section class="mt-12 mb-24 max-w-xl lg:max-w-4xl mx-auto">
+      <h2
+        class="text-[26px] sm:text-[32px] max-w-sm lg:max-w-none mx-auto font-header font-bold mb-12 lg:text-[42px] leading-tight text-center"
+      >
+        Pourquoi utiliser notre application
+      </h2>
+      <div class="grid justify-center lg:grid-cols-3 gap-6 mt-8 gap-x-16 gap-y-8">
+        <AppAdvantage iconName="mdi:secure-outline">
+          Toutes vos notes sont uniquement accessibles par vous. Personne y compris nous, ne peut voir ou accéder à vos
+          notes.
+        </AppAdvantage>
+        <AppAdvantage iconName="mdi:run-fast">
+          Tout se fait très rapidement, vous avez juste à retranscrire les groupes de notes et les notes de OGE vers
+          cette application et nous nous chargeons de tous les calculs pour vous
+        </AppAdvantage>
+        <AppAdvantage iconName="tabler:eye-star">
+          L’interface est simple d’utilisation et épurée, elle contient également un mode clair et sombre selon vos
+          préférences
+        </AppAdvantage>
+      </div>
+    </section>
+  </main>
+
 </template>
 
 <script setup>
+const email = ref("");
+const password = ref("");
 </script>
---
+++ pages/register.vue
@@ -0,0 +1,86 @@
+<template>
+  <main class="mx-4 mt-12 lg:mt-16 mb-24 min-h-[calc(100vh-280px)] lg:min-h-[calc(100vh-308px)]">
+    <section class="max-w-[360px] mx-auto">
+      <h1
+          class="text-[40px] font-header font-bold mb-4 lg:text-[42px] leading-tight"
+      >
+        Bienvenue sur Calcule Ma Moyenne
+      </h1>
+      <p class="text-xl mb-8">
+        Si c’est votre première fois sur notre application, alors vous êtes au bon endroit.
+      </p>
+      <form @submit.prevent="register">
+        <CustomInput
+            name="pseudo"
+            label="Pseudonyme"
+            type="text"
+            placeholder="Pseudonyme"
+            v-model="pseudo"
+            class="mb-4"
+            autoComplete="username"
+        />
+        <CustomInput
+            name="password"
+            label="Mot de passe"
+            type="password"
+            placeholder="Mot de passe"
+            v-model="password"
+            class="mb-4"
+            autoComplete="new-password"
+        />
+        <CustomInput
+            name="passwordConfirmation"
+            label="Répétition du mot de passe"
+            type="password"
+            placeholder="Mot de passe répété"
+            v-model="passwordConfirmation"
+            class="mb-8"
+            :error="passwordError"
+            autoComplete="new-password"
+        />
+        <div class="flex items-center gap-4">
+          <button
+              type="submit"
+              class="flex-shrink-0 bg-primary-button dark:bg-primary-button-dark text-primary-button-text dark:text-primary-button-text-dark text-lg px-4 py-3 sm:w-auto sm:mb-0"
+          >
+            S'inscrire
+          </button>
+          <NuxtLink to="/" class="hover:underline underline-offset-2">
+            J’ai déjà un compte, je me connecte
+          </NuxtLink>
+        </div>
+      </form>
+    </section>
+  </main>
+</template>
+
+<script setup>
+const pseudo = ref('');
+const password = ref('');
+const passwordConfirmation = ref('');
+
+const pseudoError = ref('');
+const passwordError = ref('');
+
+const register = async () => {
+  pseudoError.value = '';
+  passwordError.value = '';
+
+  if (password.value.length < 5) {
+    passwordError.value = 'Le mot de passe doit faire au moins 5 caractères';
+  } else if (password.value !== passwordConfirmation.value) {
+    passwordError.value = 'Les mots de passe ne sont pas identiques';
+  } else {
+    const data = await useFetch('/api/user/register', {
+      method: 'POST',
+      body: {
+        pseudo: pseudo.value,
+        password: password.value,
+      },
+      watch: false
+    });
+
+    console.log(data)
+  }
+}
+</script>
