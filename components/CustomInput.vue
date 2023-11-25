ccfd799c88f0951bbcb79111cf70e2240e2b3bf3
Valou0539 <valentin.gindre@gmail.com>
2023-11-25 12:38:56 +0100

    create CustomInput.vue component

---
+++ components/CustomInput.vue
@@ -0,0 +1,91 @@
+<template>
+  <div>
+    <label
+        :for="name"
+        class="block mb-1 text-lg lg:text-xl font-medium"
+    >
+      {{ label }}
+    </label>
+    <div class="relative">
+      <input
+          ref="input"
+          :id="name"
+          :name="name"
+          :type="!showPassword ? type : 'text'"
+          :value="modelValue"
+          :placeholder="placeholder"
+          :autocomplete="autoComplete"
+          @input="$emit('update:modelValue', $event.target.value)"
+          class="w-full bg-secondary-button dark:bg-secondary-button-dark text-base px-4 py-3"
+      />
+      <button
+          v-if="type === 'password' && !showPassword"
+          type="button"
+          @click="showPassword = !showPassword"
+          @keydown.enter="showPassword = !showPassword; input.focus()"
+          class="absolute right-2 bottom-1/2 translate-y-1/2"
+      >
+        <Icon
+            class="opacity-75 hover:opacity-100"
+            name="mdi:eye"
+            size="24px"
+        />
+      </button>
+      <button
+          v-if="type === 'password' && showPassword"
+          type="button"
+          @click="showPassword = !showPassword"
+          @keydown.enter="showPassword = !showPassword; input.focus()"
+          class="absolute right-2 bottom-1/2 translate-y-1/2"
+      >
+        <Icon
+            class="opacity-75 hover:opacity-100"
+            name="mdi:eye-off"
+            size="24px"
+        />
+      </button>
+    </div>
+    <span class="text-error dark:text-error-dark mt-1 block">
+      {{ error }}
+    </span>
+
+  </div>
+</template>
+
+<script setup>
+const props = defineProps({
+  name: {
+    type: String,
+    required: true,
+  },
+  label: {
+    type: String,
+    required: true,
+  },
+  type: {
+    type: String,
+    default: "text",
+  },
+  autoComplete: {
+    type: String,
+    default: "off",
+  },
+  modelValue: {
+    type: String,
+    default: "",
+  },
+  placeholder: {
+    type: String,
+    default: "",
+  },
+  error: {
+    type: String,
+    default: "",
+  },
+});
+
+const emit = defineEmits(["update:modelValue"]);
+
+const showPassword = ref(false);
+const input = ref(null);
+</script>
