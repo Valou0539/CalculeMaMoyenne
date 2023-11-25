d1ff89e750ae678f1fb5e1ca9f48dad5f56177dd
Valou0539 <valentin.gindre@gmail.com>
2023-11-25 12:38:41 +0100

    create AppAdvantage.vue component

---
+++ components/AppAdvantage.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="flex flex-col items-center gap-4  max-w-[240px]">
+    <Icon :name="iconName" class="w-12 h-12 text-accent" />
+    <p class="w-full text-center">
+      <slot />
+    </p>
+  </div>
+</template>
+
+<script setup>
+const props = defineProps({
+  iconName: {
+    type: String,
+    required: true,
+  },
+});
+</script>
